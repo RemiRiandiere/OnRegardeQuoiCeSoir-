83aeb214726ba4710d43feb85af062fd9da58539
RemiRiandiere <lgseasygaming@gmail.com>
2022-06-01 12:18:57 +0200

    CSS: display flexbox

--- src/app/models/film-info.model.ts
+++ src/app/models/film-info.model.ts
@@ -1,12 +1,18 @@
-import { ActorInfo } from "./actor-info.model";
+import { ActorInfo } from './actor-info.model';
 
 export class FilmInfo {
   title: string;
   synopsis: string;
   releaseDate: Date;
   imgUrl: string;
-  
-  constructor(title: string, synopsis: string, imgUrl: string, releaseDate: Date)
+
+  constructor(
+    title: string,
+    synopsis: string,
+    imgUrl: string,
+    releaseDate: Date
+  ) {
+    this.title = title;
     this.synopsis = synopsis;
     this.imgUrl = imgUrl;
     this.releaseDate = releaseDate;
