d7081be09e42f873751b41bde203f57b6732e527
RemiRiandiere <lgseasygaming@gmail.com>
2022-06-01 19:37:38 +0200

    Dependencies added: FlexLayout + implementation in code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { FilmInfoComponent } from './film-info/film-info.component';
@@ -12,7 +15,15 @@ import { LandingPageComponent } from './landing-page/landing-page-component';
 import { AppMenuComponent } from './app-menu/app-menu.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+  ],
   declarations: [
     AppComponent,
     AppMenuComponent,
